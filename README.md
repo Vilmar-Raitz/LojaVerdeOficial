a6cd05e2b4e1da84e73b092f32cbb7f2823b3cec
Vilmar-Raitz <vilmarraitz.10@gmail.com>
2023-12-01 18:37:43 -0400

    projeto finalizado

--- README.md
+++ README.md
@@ -1,36 +1,37 @@
 # loja_verde
 
 > Atividades
-1. [x] Criar o banco de dados da Loja
-2. [x] Criar a estrutura de pastas do projeto
-3. [x] Adicionar o framework de CSS
-4. [x] Criar Classe de Conexão com Banco de dados
-5. [x] Criar a Classe da Camada DAO (ProdutoDAO)
- 1. [x] Salvar Produto
- 2. [x] Pesquisar Produto
- 3. [x] Pesquisar Produto ID
- 4. [ ] Alterar Produto
- 5. [ ] Excluir Produto
-6. [x] Criar a Classe de Modelo (Produto)
-7. [x] Criar o Core (Núcleo da Aplicação)
-   1. [x] Criar o App.php (Carrega os Controles)
-   2. [x] Criar a Classe Controller (Carrega os models)
-   3. [x] Criar o autoload (Carrega os arquivos na memória)
-8. [x] Criar o HomeController (Carregar a Index)
-9. [x] Criar o ProdutoController (Controla as operações de Produto)
-   1. [x] Criar a Index (View)
-   2. [x] Cria o Menu (Layout)
-   3. [x] Criar Views de Produto
-   4. [x] Cadastrar Produto
-   5. [x] Index Produto (Listar Produto)
-   6. [x] Editar Produto
-10. [ ] Funcionalidade Excluir Produto [0.5]
-11. [ ] Cadastrar Usuário [0.5]
-12. [ ] Pesquisar Usuário [0.5]
-13. [ ] Alterar Usuário [0.5]
-14. [ ] Excluir Usuário [0.5]
-15. [ ] Efetuar Login/Logout do Usuário [1.0]
-16. [ ] Listar Produtos Na página Inicial [1.0]
-17. [ ] Listar Produtos com Imagem [2.0]
-18. [ ] Adicionar o Modal de Confirmação de exclusão no projeto[1.0]
-19. [ ] Refatorar o ProdutoDAO com PreparedStatement ou PDO [1.0]
+
+1. [X] Criar o banco de dados da Loja
+2. [X] Criar a estrutura de pastas do projeto
+3. [X] Adicionar o framework de CSS
+4. [X] Criar Classe de Conexão com Banco de dados
+5. [X] Criar a Classe da Camada DAO (ProdutoDAO)
+6. [X] Salvar Produto
+7. [X] Pesquisar Produto
+8. [X] Pesquisar Produto ID
+9. [X] Alterar Produto
+1. [X] Excluir Produto
+1. [X] Criar a Classe de Modelo (Produto)
+1. [X] Criar o Core (Núcleo da Aplicação)
+     1. [X] Criar o App.php (Carrega os Controles)
+     2. [X] Criar a Classe Controller (Carrega os models)
+     3. [X] Criar o autoload (Carrega os arquivos na memória)
+1. [X] Criar o HomeController (Carregar a Index)
+1. [X] Criar o ProdutoController (Controla as operações de Produto)
+     1. [X] Criar a Index (View)
+     2. [X] Cria o Menu (Layout)
+     3. [X] Criar Views de Produto
+     4. [X] Cadastrar Produto
+     5. [X] Index Produto (Listar Produto)
+     6. [X] Editar Produto
+1. [X] Funcionalidade Excluir Produto [0.5]
+1. [X] Cadastrar Usuário [0.5]
+1. [X] Pesquisar Usuário [0.5]
+1. [X] Alterar Usuário [0.5]
+1. [X] Excluir Usuário [0.5]
+2. [X] Efetuar Login/Logout do Usuário [1.0]
+2. [X] Listar Produtos Na página Inicial [1.0]
+2. [X] Listar Produtos com Imagem [2.0]
+2. [ ] Adicionar o Modal de Confirmação de exclusão no projeto[1.0]
+2. [X] Refatorar o ProdutoDAO com PreparedStatement ou PDO [1.0]
--- application/views/usuario/index.php
+++ application/views/usuario/index.php
@@ -1,21 +1,14 @@
 <?php
 $base = __DIR__;
 include $base . '\..\layout\menu.php';
-
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" href="index.css">
+<title>Lista de Usuários</title>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
-
-   
-
-    <title>Lista de Produtos</title>
 </head>
 <style>
     body {
@@ -81,61 +74,70 @@ include $base . '\..\layout\menu.php';
 </style>
 
 <body>
-    <h1>Lista de Produtos</h1>
-    <?php if (isset($data['msg-deletar'])) { ?>
-        <div class="alert alert-danger" role="alert">Produto deletado com sucesso</div>
+
+    <form action="/usuario/buscar" method="POST" style="margin-bottom: 20px;">
+        <label for="search">Buscar Usuário:</label>
+        <input type="text" name="nome" placeholder="Digite o nome do usuário">
+        <input type="submit" value="Buscar">
+    </form>
+
+
+    <h1>Lista de Usuários</h1>
+    <?php if (isset($data['msg'])) { ?>
+        <div class="alert alert-danger" role="alert">Usuário removido com Sucesso</div>
     <?php } ?>
-    <p><a href="/produto/cadastrar" class="acao-botao-azul">Adicionar Produto</a></p>
+    <p><a href="/usuario/cadastrar" class="acao-botao-azul">Adicionar Usuário</a></p>
     <table>
         <thead>
             <th>Código</th>
             <th>Nome</th>
-            <th>Marca</th>
-            <th>Preço</th>
-            <th>Imagem</th>
+            <th>CPF</th>
+            <th>E-mail</th>
             <th>Ações</th>
         </thead>
         <tbody>
-            <?php foreach ($data['produtos'] as $produto) { ?>
+            <?php foreach ($data['usuarios'] as $usuario) { ?>
                 <tr>
-                    <td><?= $produto->getCodigo() ?></td>
-                    <td><?= $produto->getNome() ?></td>
-                    <td><?= $produto->getMarca() ?></td>
-                    <td><?= $produto->getPreco() ?></td>
-                    <td><img src="<?= $produto->getImagem() ?>" alt="Sem imagem" style="width: 150px;  height: auto"></td>
+                    <td><?= $usuario->getCodigo() ?></td>
+                    <td><?= $usuario->getNome() ?></td>
+                    <td><?= $usuario->getCpf() ?></td>
+                    <td><?= $usuario->getEmail() ?></td>
                     <td>
+                        <a href="/usuario/iniciarEditar/<?= $usuario->getCodigo() ?>" class="acao-botao-azul">Editar Usuário</a>
 
-                        <a href="/produto/iniciarEditar/<?= $produto->getCodigo() ?>" class="acao-botao-azul">Editar</a>
 
-                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#confirmationModal<?= $produto->getCodigo() ?>">
-                            Excluir
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#confirmationModal<?= $usuario->getCodigo() ?>">
+                            Excluir Usuário
                         </button>
 
-
-                        <div class="modal fade" id="confirmationModal<?= $produto->getCodigo() ?>" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel<?= $produto->getCodigo() ?>" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered" role="document">
+                        <!-- Modal -->
+                        <div class="modal fade" id="confirmationModal<?= $usuario->getCodigo() ?>" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel<?= $usuario->getCodigo() ?>" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title" id="confirmationModalLabel<?= $produto->getCodigo() ?>">Confirmação</h5>
+                                        <h5 class="modal-title" id="confirmationModalLabel<?= $usuario->getCodigo() ?>">Confirmação de Exclusão</h5>
                                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
                                     </div>
                                     <div class="modal-body">
-                                        <p>Tem certeza de que deseja excluir o produto?</p>
+                                        <p>Tem certeza de que deseja excluir o usuário?</p>
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                                        <button type="button" class="btn btn-danger" onclick="document.getElementById('deleteForm<?= $produto->getCodigo() ?>').submit()">Excluir</button>
+                                        <button type="button" class="btn btn-danger" onclick="document.getElementById('deleteUsuarioForm<?= $usuario->getCodigo() ?>').submit()">Excluir</button>
                                     </div>
                                 </div>
                             </div>
                         </div>
 
-                        <!-- Formulário de exclusão -->
-                        <form id="deleteForm<?= $produto->getCodigo() ?>" action="/produto/deletar" method="POST">
-                            <input type="hidden" value="<?= $produto->getCodigo() ?>" name="codigo" />
+                        <!-- Formulário de exclusão de usuário -->
+                        <form id="deleteUsuarioForm<?= $usuario->getCodigo() ?>" action="/usuario/deletar" method="POST">
+                            <input type="hidden" value="<?= $usuario->getCodigo() ?>" name="codigo" />
                         </form>
+
+
 
                     </td>
                 </tr>
